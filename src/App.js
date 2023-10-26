58ce868667a9aa4bcb8c9768d4a6992e93615fa9
Wallace Leandro <wallaceleandro2003@gmail.com>
2023-10-26 17:03:34 -0300

    Scroll up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import Header from "./components/Header/Header"
 import Home from './Pages/Home/Home'
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React, { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    const handleScrollToTop = () => {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+
+    const links = document.querySelectorAll('a');
+    links.forEach(link => {
+      link.addEventListener('click', handleScrollToTop); // Subir para o topo das páginas
+    });
+
+    return () => {
+      links.forEach(link => {
+        link.removeEventListener('click', handleScrollToTop);
+      });
+    };
+  }, []);
+
   return (
     <div className="App">
         <Header /> 
